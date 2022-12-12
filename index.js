e03c88b397ef6df6fbea07a1b562a33d4e8c4bcd
Vikram Srinivas <vikramksm@hotmail.com>
2022-12-12 08:57:18 +0000

    js update

---
+++ index.js
@@ -0,0 +1,31 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [5405],
+  {
+    8312: function (n, u, _) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/",
+        function () {
+          return _(4186);
+        },
+      ]);
+    },
+    4186: function (n, u, _) {
+      "use strict";
+      _.r(u),
+        _.d(u, {
+          __N_SSP: function () {
+            return f;
+          },
+        });
+      var t = _(5606),
+        f = !0;
+      u.default = t.f;
+    },
+  },
+  function (n) {
+    n.O(0, [7766, 8812, 5606, 9774, 2888, 179], function () {
+      return n((n.s = 8312));
+    }),
+      (_N_E = n.O());
+  },
+]);
